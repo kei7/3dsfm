0bc210c8ebf460e0b3eda20b8a30449530090f75
kei7 <kei950120@gmail.com>
2019-03-26 20:13:14 +0900

    modify exec dir

--- install/exec_sfm.sh
+++ install/exec_sfm.sh
@@ -13,7 +13,7 @@ MVGEXEC="src/software/SfM"
 mkdir ${MVGBUILDMAIN}/${MVGEXEC}/input
 #cp ./model.py.in ./make_model.py.in
 cat ./model.py.in | sed -e "s#@OPENMVG_SOFTWARE_SFM_BUILD_DIR@#/home/repos/openMVG_build/Linux-x86_64-RELEASE#g" -e "s#@OPENMVG_SOFTWARE_SFM_SRC_DIR#/home/repos/openMVG/src/software/SfM#g" -e "s#@IMGSRC@#${IMGDIRNAME}#g" > ./${MAKEMODEL_NAME}.py
-mv ./${MAKEMODEL_NAME}.py ../openMVG/src/software/SfM/
+mv ./${MAKEMODEL_NAME}.py ${MVGBUILDMAIN}/${MVGEXEC}/
 cp -r ${IMGDIRPATH} ${MVGBUILDMAIN}/${MVGEXEC}/input/
 cp -r ${IMGDIRPATH} ${MVSBUILDMAIN}/undistorted_images/
 #execute
