1dd247c58289764e99a1848e791c897cfb5aa6a5
kei7 <kei950120@gmail.com>
2019-09-17 21:54:02 +0900

    change shell

--- dev_mvg/link.sh
+++ dev_mvg/link.sh
@@ -2,4 +2,5 @@
 
 cp -f ./main_IncrementalSfM.cpp ../../openMVG/src/software/SfM/
 cp -f ./Scene.cpp ../../openMVS/libs/MVS/
-cp -f ./DensifyPointCloud.cpp ../../openMVS/apps/
+#cp -f ./DensifyPointCloud.cpp ../../openMVS/apps/DensifyPointCloud/
+cp -f ./SceneTexture.cpp ../../openMVS/libs/MVS/
