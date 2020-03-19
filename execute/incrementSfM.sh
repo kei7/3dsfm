0eb0045155c9e6878e46a040d943d8cd6ca699d1
kei7 <kei950120@gmail.com>
2020-03-19 09:30:44 +0900

    Update incrementSfM.sh

--- execute/incrementSfM.sh
+++ execute/incrementSfM.sh
@@ -9,7 +9,7 @@ else
   mkdir ${MVGBUILDMAIN}/${MVGEXEC}/input
 fi
 
-cat ./model3.py.in | sed -e "s#@OPENMVG_SOFTWARE_SFM_BUILD_DIR@#/home/repos/openMVG_0120_build/Linux-x86_64-RELEASE#g" -e "s#@OPENMVG_SOFTWARE_SFM_SRC_DIR@#/home/repos/openMVG/src/software/SfM#g" -e "s#@IMGSRC@#${IMGDIRNAME}#g" > ./${MAKEMODEL_NAME}3.py
+cat ./model3.py.in | sed -e "s#@OPENMVG_SOFTWARE_SFM_BUILD_DIR@#/home/repos/openMVG_build/Linux-x86_64-RELEASE#g" -e "s#@OPENMVG_SOFTWARE_SFM_SRC_DIR@#/home/repos/openMVG/src/software/SfM#g" -e "s#@IMGSRC@#${IMGDIRNAME}#g" > ./${MAKEMODEL_NAME}3.py
 mv ./${MAKEMODEL_NAME}3.py ${MVGBUILDMAIN}/${MVGEXEC}/
 
 python ${MVGBUILDMAIN}/${MVGEXEC}/${MAKEMODEL_NAME}3.py
