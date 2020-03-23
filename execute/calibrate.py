1a58e9b36a319649a2dff3b1b2e63a4dc25cb713
kei7 <kei950120@gmail.com>
2020-03-23 10:17:31 +0900

    Update calibrate.py

--- execute/calibrate.py
+++ execute/calibrate.py
@@ -14,28 +14,57 @@ obj_points = []
 img_points = []
 
 filesa = sorted(glob.glob(os.path.join(src_patha,'*.png')))
-for fn in filesa:
-    # 画像の取得
-    im = cv2.imread(fn, 0)
-    print("loading..." + fn)
-    # チェスボードのコーナーを検出
-    found, corner = cv2.findChessboardCorners(im, pattern_size)
-    # コーナーがあれば
-    if found:
-        term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
-        cv2.cornerSubPix(im, corner, (5,5), (-1,-1), term)
-    # コーナーがない場合のエラー処理
-    if not found:
-        print('chessboard not found')
-        continue
-    img_points.append(corner.reshape(-1, 2))   #appendメソッド：リストの最後に因数のオブジェクトを追加
-    obj_points.append(pattern_points)
-    #corner.reshape(-1, 2) : 検出したコーナーの画像内座標値(x, y)
+if len(filesa) >0:
+    for fn in filesa:
+        # 画像の取得
+        im = cv2.imread(fn, 0)
+        print("loading..." + fn)
+        # チェスボードのコーナーを検出
+        found, corner = cv2.findChessboardCorners(im, pattern_size)
+        # コーナーがあれば
+        if found:
+            term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
+            cv2.cornerSubPix(im, corner, (5,5), (-1,-1), term)
+        # コーナーがない場合のエラー処理
+        if not found:
+            print('chessboard not found')
+            continue
+        img_points.append(corner.reshape(-1, 2))   #appendメソッド：リストの最後に因数のオブジェクトを追加
+        obj_points.append(pattern_points)
+        #corner.reshape(-1, 2) : 検出したコーナーの画像内座標値(x, y)
 
-# 内部パラメータを計算
-rms, K, d, r, t = cv2.calibrateCamera(obj_points,img_points,(im.shape[1],im.shape[0]),None,None)
-# 計算結果を表示
-print("camera opt a")
-print ("RMS = ", rms)
-print ("K = \n", K)
-print ("d = ", d.ravel())
+    # 内部パラメータを計算
+    rms, K, d, r, t = cv2.calibrateCamera(obj_points,img_points,(im.shape[1],im.shape[0]),None,None)
+    # 計算結果を表示
+    print("camera opt a")
+    print ("RMS = ", rms)
+    print ("K = \n", K)
+    print ("d = ", d.ravel())
+else:
+    filesa = sorted(glob.glob(os.path.join(src_patha,'*.jpg')))
+    
+    for fn in filesa:
+        # 画像の取得
+        im = cv2.imread(fn, 0)
+        print("loading..." + fn)
+        # チェスボードのコーナーを検出
+        found, corner = cv2.findChessboardCorners(im, pattern_size)
+        # コーナーがあれば
+        if found:
+            term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
+            cv2.cornerSubPix(im, corner, (5,5), (-1,-1), term)
+        # コーナーがない場合のエラー処理
+        if not found:
+            print('chessboard not found')
+            continue
+        img_points.append(corner.reshape(-1, 2))   #appendメソッド：リストの最後に因数のオブジェクトを追加
+        obj_points.append(pattern_points)
+        #corner.reshape(-1, 2) : 検出したコーナーの画像内座標値(x, y)
+
+    # 内部パラメータを計算
+    rms, K, d, r, t = cv2.calibrateCamera(obj_points,img_points,(im.shape[1],im.shape[0]),None,None)
+    # 計算結果を表示
+    print("camera opt a")
+    print ("RMS = ", rms)
+    print ("K = \n", K)
+    print ("d = ", d.ravel())
